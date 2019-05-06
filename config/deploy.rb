4176d96e50e3e358677d9b259ebe383753a3b5c3
zumi-n <zumi0369@gmail.com>
2019-05-06 18:36:01 +0900

    Edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/secrets.yml }
+set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
