94429426a061a99ebb1e65a5af4d80b81c9ac941
zumi-n <zumi0369@gmail.com>
2019-05-06 14:39:14 +0900

    Edit migration

--- db/migrate/20190427092438_add_age_to_profile.rb
+++
@@ -1,7 +0,0 @@
-class AddAgeToProfile < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :department, :string
-    add_column :profiles, :age, :integer
-    add_column :profiles, :joined_year, :date
-  end
-end
